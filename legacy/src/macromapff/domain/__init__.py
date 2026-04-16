8a85423b380f2e5e98b8475b5f0eed310827b18c
zhangz <858978567@qq.com>
2026-04-16 14:41:26 +0800

    no legacy

--- legacy/src/macromapff/domain/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Domain package namespace.
-
-Internal code should import concrete modules directly.
-"""
-
-__all__ = []
--- legacy/src/macromapff/io/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""IO package namespace.
-
-Internal code should import concrete modules directly.
-"""
-
-__all__ = []
