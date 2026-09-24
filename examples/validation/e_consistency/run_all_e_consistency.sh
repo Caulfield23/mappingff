4d1fa1cf39691a93224290cdfbfe6e97ff96c4e9
zhangz <858978567@qq.com>
2026-09-24 14:56:51 +0800

    Move examples to a separate submodule

--- examples/validation/e_consistency/run_all_e_consistency.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for dir in /home/zhangzhao/zhangzhao/Projects/mappingff_run/validation/e_consistency/ligpargen/*/; do
-    cd "$dir" && lmp -in in_e_consistency.lammps -var datafile *.lmp
-done
-
-for dir in /home/zhangzhao/zhangzhao/Projects/mappingff_run/validation/e_consistency/mappingff/*/; do
-    cd "$dir" && lmp -in in_e_consistency.lammps -var datafile *.data
-done
